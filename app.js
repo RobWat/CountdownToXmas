a5a9c6e61cbbb533d9a7c6994b9087d54db96995
RobWat <find@A5-004015.localdomain>
2018-11-28 17:17:51 -0700

    starter code

---
+++ app.js
@@ -0,0 +1,12 @@
+// countdown to December 25
+
+// the date for today
+
+// the we want (dec 25)
+
+// calculation, to know how much longer it is till this date
+
+// display the results of the calculation
+
+
+// eventlistener, onchange, the change of the difference calculation
